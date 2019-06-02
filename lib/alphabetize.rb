9ed7eecef46e6edc5ec46457836dbf97f7ff67fd
Justin Hutsell <hutselljustin@gmail.com>
2019-06-02 16:21:18 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -5,7 +5,7 @@ ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 def alphabetize(arr)
   # code here
   arr.sort_by do |word|
-    word.split(" ").collect do |char|
+    word.split(" ").map do |char|
       ESPERANTO_ALPHABET.index(char)
     end
   end
